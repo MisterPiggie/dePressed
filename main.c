642beff5bb5c60b93cb11a02b29e3a52a314ce1f
piggie <matvey00014@gmail.com>
2026-07-03 00:14:07 +0500

    save progress

--- main.c
+++ main.c
@@ -11,28 +11,6 @@ int main(void)
     app.arena = arena_create(MB(100), KB(32));
     app.keyboards_count = 0;
 
-    if (!HID_get_suitable_keyboards(&app))
-    {
-        printf("no keybords were found\n");
-        return -1;
-    }
-
-    for (int i = 0; i < app.keyboards_count; i++)
-    {
-        printf("Name: %ls\n", app.keyboards[i].product_name);
-        printf("Path: %s\n", app.keyboards[i].path);
-    }
-
-    for (int i = 0; i < app.keyboards_count; i++)
-    {
-        app.active_model_idx = i;
-        if(HID_open_device(&app.keyboards[i]))
-        {
-            printf("Connected\n");
-            break;
-        }
-    }
-
 
     printf("Protocol version:\n");
     VIA_get_protocol_version(&app.keyboards[app.active_model_idx]);
